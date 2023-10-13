dbbc0e30902f105f4952ea979f43d0309f042d97
Johan Paredes <jmpareds@gmail.com>
2023-10-13 00:23:49 -0400

    first solution to 746

--- 746MinCostClimbingStairs.cpp
+++ 746MinCostClimbingStairs.cpp
@@ -5,21 +5,19 @@ using namespace std;
 class Solution {
 public:
     int minCostClimbingStairs(vector<int>& cost) {
-        int sum = 0;
-        int n = cost.size();
-        int last_index = n;
-
-        while (last_index > 1){
-            if( cost[last_index - 2] > cost[last_index - 1] ){
-                sum += cost[last_index - 1];
-                last_index -= 1;
-            } else {
-                sum += cost[last_index - 2];
-                last_index -= 2;
-            }
+        for( int i = cost.size() - 3; i >= 0; i--){
+            cost[i] += min(cost[i + 1], cost[i + 2]);
         }
 
-        return sum;
+        return min(cost[0], cost[1]);
+    }
+
+    int min (int a, int b){
+        if (a < b){
+            return a;
+        } else {
+            return b;
+        }
     }
 };
 

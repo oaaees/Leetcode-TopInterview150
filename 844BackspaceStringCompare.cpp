25d177516af143fc3c1367add8df8012db2feb54
Johan Paredes <jmpareds@gmail.com>
2023-10-20 23:45:06 -0400

    finished 844 Backspace String Compare

--- 844BackspaceStringCompare.cpp
+++ 844BackspaceStringCompare.cpp
@@ -1,21 +1,38 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int i = s.size() - 1;
-        int j = t.size() - 1;
-
-
-
-
-        return true;
-    }
-    int count_char(string s, char c){
-        int count = 0;
+        string res_s = "";
+        string res_t = "";
 
         for(int i = 0; i < s.size(); i++){
-            if(s[i] == c) count++;
+            if (s[i] == '#'){
+                if (res_s.size() > 0) res_s.pop_back();
+            } else {
+                res_s.push_back(s[i]);
+            }
         }
 
-        return count;
+        for(int i = 0; i < t.size(); i++){
+            if (t[i] == '#'){
+                if (res_t.size() > 0) res_t.pop_back();
+            } else {
+                res_t.push_back(t[i]);
+            }
+        }
+
+        return res_s == res_t;
     }
 };
+
+int main(){
+
+    Solution sol;
+
+    cout << sol.backspaceCompare("ab#c", "ad#c");
+
+    return 0;
+}

7f37b02ef3e29f4da082825acfc5d40143ccb960
Johan Paredes <jmpareds@gmail.com>
2023-10-28 00:55:05 -0400

    finished 1220

--- 1220CountVowelsPermutation.cpp
+++ 1220CountVowelsPermutation.cpp
@@ -5,27 +5,29 @@ using namespace std;
 class Solution {
 public:
     int countVowelPermutation(int n) {
-        vector<vector<int>> dp(5, vector<int>(n, 0));
+        vector<vector<int>> dp(n, vector<int>(5, 1));
     	
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = 0; j < 5; j++){
-                
-            }
+        for(int i = 1; i < n; i++){
+            dp[i][0] = (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4]) % 1000000007;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 1000000007;
+            dp[i][2] = (dp[i - 1][1] + dp[i - 1][3]) % 1000000007;
+            dp[i][3] = (dp[i - 1][2]) % 1000000007;
+            dp[i][4] = (dp[i - 1][2] + dp[i - 1][3]) % 1000000007;
         }
 
         long long sum = 0;
 
         for (int i = 0; i < 5; i++){
-            sum += dp[i][0];
+            sum += dp[n - 1][i];
         }
 
         return sum;
     }
 };
 
-int main (){
+int main (int argc, char *argv[]){
     Solution sol;
-    cout << sol.countVowelPermutation(1);
+    cout << sol.countVowelPermutation(stoi(argv[1]));
 
     return 0;
 }

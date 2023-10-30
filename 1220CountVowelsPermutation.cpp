b10ad1ea5884768738db0bdeb7625bc7c625449f
Johan Paredes <jmpareds@gmail.com>
2023-10-30 11:19:03 -0400

    added mod rule to 1220

--- 1220CountVowelsPermutation.cpp
+++ 1220CountVowelsPermutation.cpp
@@ -6,13 +6,14 @@ class Solution {
 public:
     int countVowelPermutation(int n) {
         vector<vector<int>> dp(n, vector<int>(5, 1));
+        int mod = 1000000007;
     	
         for(int i = 1; i < n; i++){
-            dp[i][0] = (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4]) % 1000000007;
-            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 1000000007;
-            dp[i][2] = (dp[i - 1][1] + dp[i - 1][3]) % 1000000007;
-            dp[i][3] = (dp[i - 1][2]) % 1000000007;
-            dp[i][4] = (dp[i - 1][2] + dp[i - 1][3]) % 1000000007;
+            dp[i][0] = (dp[i - 1][1] + dp[i - 1][2] + dp[i - 1][4]) % mod;
+            dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % mod;
+            dp[i][2] = (dp[i - 1][1] + dp[i - 1][3]) % mod;
+            dp[i][3] = (dp[i - 1][2]) % mod;
+            dp[i][4] = (dp[i - 1][2] + dp[i - 1][3]) % mod;
         }
 
         long long sum = 0;
@@ -21,7 +22,7 @@ public:
             sum += dp[n - 1][i];
         }
 
-        return sum;
+        return sum % mod;
     }
 };
 
